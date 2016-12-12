b1d0410aacbabc4c35264a46706f9cd35bc4d8ef
MacBook Pro <Apple@MacBooks-MacBook-Pro.local>
2016-12-12 16:30:27 +0330

    initial

---
+++ y chize sade/y chize sade/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  y chize sade
+//
+//  Created by MacBook Pro on 12/12/16.
+//  Copyright © 2016 MacBook Pro. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
