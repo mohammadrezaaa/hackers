4307d78052d65a6457869c2c7d0644a70da98e86
MacBook Pro <Apple@MacBooks-MacBook-Pro.local>
2016-12-20 12:53:46 +0330

    sdfghjkl;

---
+++ midooni/midooni/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  midooni
+//
+//  Created by MacBook Pro on 12/20/16.
+//  Copyright © 2016 MacBook Pro. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("midooni midooni\n");
+  
+}
